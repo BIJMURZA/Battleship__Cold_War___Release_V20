2a3a1932b6e45f5d4eb57d558914936480340dc5
BIJMURZA <Venom_Skill@outlook.com>
2023-05-08 21:31:34 +0700

    Initial commit

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ document.getElementById("buttonRules").onclick = function () {
     document.getElementById("modalRules").style.display = "block";
 }
 
+document.getElementById("buttonSettings").onclick = function () {
+    document.getElementById("modalSettings").style.display = "block";
+}
+
+document.getElementById("saveSettings").addEventListener("click", function() {
+    if (document.getElementById('volumeSwitch').checked === true) {
+        document.getElementById("audioTheme").play();
+    } else {
+        document.getElementById("audioTheme").pause();
+    }
+    document.getElementById('modalSettings').style.display='none';
+})
+
 window.onclick = function(event) {
     if (event.target === document.getElementById('modalRules')) {
         document.getElementById('modalRules').style.display = 'none';
